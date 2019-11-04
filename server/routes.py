02c0b67af9414eb1a5bbc269ac7b288055fdf467
tukva <vanton@KH6805.softservecom.com>
2019-11-04 12:00:04 +0200

    Fix routes

--- server/routes.py
+++ server/routes.py
@@ -6,7 +6,7 @@ def add_routes(app):
     app.register_listener(acquire_con, "before_server_start")
     app.register_listener(close_con, "after_server_stop")
 
-    app.add_route(ParserAllLinksView.as_view(), '/parse-links/<name>')
-    app.add_route(ParserLinkView.as_view(), '/parse-links/<link_id:int>/<name>')
+    app.add_route(ParserAllLinksView.as_view(), '/parse-links/teams')
+    app.add_route(ParserLinkView.as_view(), '/parse-links/<link_id:int>/teams')
 
     app.add_route(RealTeamView.as_view(), '/real-team')
--- server/services/views/parse_link.py
+++ server/services/views/parse_link.py
@@ -12,54 +12,50 @@ from services.forms import TeamResponseSchema
 
 class ParserLinkView(HTTPMethodView):
 
-    async def get(self, request, link_id, name):
-        if name == 'teams':
-            async with Connection() as conn:
-                teams = await conn.execute(tb_team.select().where(tb_team.c.link_id == link_id))
-                res = TeamResponseSchema().dump(await teams.fetchall(), many=True)
-                return json(res, 200)
+    async def get(self, request, link_id):
+        async with Connection() as conn:
+            teams = await conn.execute(tb_team.select().where(tb_team.c.link_id == link_id))
+            res = TeamResponseSchema().dump(await teams.fetchall(), many=True)
+            return json(res, 200)
 
-    async def put(self, request, link_id, name):
-        if name == 'teams':
-            async with Connection() as conn:
-                select_tb_link = await conn.execute(tb_link.select().where(tb_link.c.link_id == link_id))
-                link = await select_tb_link.fetchone()
+    async def put(self, request, link_id):
+        async with Connection() as conn:
+            select_tb_link = await conn.execute(tb_link.select().where(tb_link.c.link_id == link_id))
+            link = await select_tb_link.fetchone()
 
-                teams = team_parser(link.link, link.attributes["cls"], link.attributes["elem"])
-                for team in teams:
-                    select_tb_team = await conn.execute(tb_team.select().where(and_(
-                        tb_team.c.name == team,
-                        tb_team.c.link_id == link_id
-                    )))
-                    exist_record = await select_tb_team.fetchone()
+            teams = team_parser(link.link, link.attributes["cls"], link.attributes["elem"])
+            for team in teams:
+                select_tb_team = await conn.execute(tb_team.select().where(and_(
+                    tb_team.c.name == team,
+                    tb_team.c.link_id == link_id
+                )))
+                exist_record = await select_tb_team.fetchone()
 
-                    if exist_record:
-                        await conn.execute(tb_team.update().where(and_(
-                            tb_team.c.name == team, tb_team.c.link_id == link_id)).values(
-                            name=team, site_name=link.site_name, created_on=datetime.utcnow(), link_id=link_id)
-                        )
-                        break
-                    else:
-                        await conn.execute(tb_team.insert().values(
-                            name=team, site_name=link.site_name, created_on=datetime.utcnow(), link_id=link_id)
-                        )
-                return text("Ok", 200)
+                if exist_record:
+                    await conn.execute(tb_team.update().where(and_(
+                        tb_team.c.name == team, tb_team.c.link_id == link_id)).values(
+                        name=team, site_name=link.site_name, created_on=datetime.utcnow(), link_id=link_id)
+                    )
+                    break
+                else:
+                    await conn.execute(tb_team.insert().values(
+                        name=team, site_name=link.site_name, created_on=datetime.utcnow(), link_id=link_id)
+                    )
+            return text("Ok", 200)
 
-    async def delete(self, request, link_id, name):
-        if name == 'teams':
-            async with Connection() as conn:
-                await conn.execute(tb_team.delete().where(tb_team.c.link_id == link_id))
-                return text("Ok", 200)
+    async def delete(self, request, link_id):
+        async with Connection() as conn:
+            await conn.execute(tb_team.delete().where(tb_team.c.link_id == link_id))
+            return text("Ok", 200)
 
 
 class ParserAllLinksView(HTTPMethodView):
 
-    async def get(self, request, name):
-        if name == 'teams':
-            async with Connection() as conn:
-                teams = await conn.execute(tb_team.select())
-                res = TeamResponseSchema().dump(await teams.fetchall(), many=True)
-                return json(res, 200)
+    async def get(self, request):
+        async with Connection() as conn:
+            teams = await conn.execute(tb_team.select())
+            res = TeamResponseSchema().dump(await teams.fetchall(), many=True)
+            return json(res, 200)
 
 
 class RealTeamView(HTTPMethodView):
