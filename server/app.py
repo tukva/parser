d56b32ea986e1c46fc4f2d2c4448ac9dee299417
tukva <vanton@KH6805.softservecom.com>
2019-11-20 14:34:23 +0200

    Change config.py

--- server/app.py
+++ server/app.py
@@ -1,9 +1,10 @@
 from sanic import Sanic
 
+from config import PARSER_API_HOST
 from routes import add_routes
 
 app = Sanic(name=__name__)
 add_routes(app)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=PARSER_API_HOST)
