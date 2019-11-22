7083fb19c744ae33f66fe6232803ad1f0caf9dee
tukva <vanton@KH6805.softservecom.com>
2019-11-22 10:32:54 +0200

    Update betagr_common

--- server/app.py
+++ server/app.py
@@ -1,10 +1,10 @@
 from sanic import Sanic
 
-from config import PARSER_API_HOST
+from config import PARSER_API_PORT
 from routes import add_routes
 
 app = Sanic(name=__name__)
 add_routes(app)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=PARSER_API_HOST)
+    app.run(host='0.0.0.0', port=PARSER_API_PORT)
