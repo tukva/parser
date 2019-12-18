90dcb4ea674a6b7fd5465c00aa5b97a03bdef47f
tukva <vanton@KH6805.softservecom.com>
2019-12-18 17:40:42 +0200

    Change loop

--- server/app.py
+++ server/app.py
@@ -1,11 +1,12 @@
 import asyncio
 
-from services.utils import update_teams
+from services.utils import create_teams, create_real_teams
 
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
-    loop.create_task(update_teams())
+    loop.create_task(create_teams())
+    loop.create_task(create_real_teams())
     try:
         loop.run_forever()
     finally:
--- server/services/utils.py
+++ server/services/utils.py
@@ -1,5 +1,4 @@
 import asyncio
-from datetime import datetime
 
 from common.rest_client.base_client_betting_data import BaseClientBettingData
 
@@ -8,23 +7,26 @@ from services.parsers import team_parser
 
 async def parse_teams_by_link(link):
     teams = await team_parser(link["link"], link["attributes"]["class"], link["attributes"]["elem"])
-    return link, teams
+    return teams, link
 
 
-async def update_teams():
+async def create_real_teams():
     client = BaseClientBettingData()
-    links = await client.get_links()
+    link = await client.get_links(where="type:eq:real_team")
+    real_teams, _ = await parse_teams_by_link(link)
+    for real_team in real_teams:
+        await client.create_real_teams(json={"name": real_team})
+    await asyncio.sleep(86400)
+    await create_real_teams()
+
+
+async def create_teams():
+    client = BaseClientBettingData()
+    links = await client.get_links(where="type:eq:team")
     futures = [parse_teams_by_link(link) for link in links]
     for future in asyncio.as_completed(futures):
-        link, teams = await future
-        tasks = []
-        if link["type"] == "real_team":
-            for team in teams:
-                tasks.append(asyncio.ensure_future(client.create_real_teams(json={"name": team})))
-        else:
-            for team in teams:
-                tasks.append(asyncio.ensure_future(client.create_team(
-                    json={"name": team, "site_name": link["site_name"], "link_id": link["link_id"]})))
-        await asyncio.wait(tasks)
+        teams, link = await future
+        for team in teams:
+            await client.create_team(json={"name": team, "site_name": link["site_name"], "link_id": link["link_id"]})
     await asyncio.sleep(7200)
-    await update_teams()
+    await create_teams()
